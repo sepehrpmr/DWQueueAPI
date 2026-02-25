3e0094796762872b54fdfce2801b6c3a9e711d47
sepehrpmr <sepehr.pmreza@gmail.com>
2026-02-25 11:50:33 +0330

    new version 2.0

---
+++ Controllers/EmployeesController .cs
@@ -0,0 +1,65 @@
+﻿using DWQueueAPI.Data.Entities;
+using DWQueueAPI.Sevices;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DWQueueAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        private readonly EmployeeService _employeeService;
+
+        public EmployeesController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var employees = await _employeeService.GetAllEmployeesAsync();
+            return Ok(employees);
+        }
+
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByID(int id)
+        {
+            var employee = await _employeeService.GetEmployeeByIDAsync(id);
+            if (employee == null)
+            {
+                return NotFound("Employee دot found");
+            }
+            return Ok(employee);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async Task<IActionResult> Add(Employees employee)
+        {
+            await _employeeService.AddEmployeeAsync(employee);
+            return Ok("Employee added successfully");
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Employees employee)
+        {
+            await _employeeService.UpdateEmployeeAsync(employee);
+            return Ok("Employee pdated successfully");
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _employeeService.DeleteEmployeeAsync(id);
+            return Ok("Employee deleted successfully");
+        }
+    }
+}
